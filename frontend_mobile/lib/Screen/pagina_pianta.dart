80e84aa053dcbae2995eb15a9ad875a9f2ac9e60
PasqualeSorrentino <119622217+PasqualeSorrentino@users.noreply.github.com>
2024-11-19 11:43:45 +0100

    creazione pagina pianta

---
+++ frontend_mobile/lib/Screen/pagina_pianta.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PaginaPianta extends StatelessWidget {
+  const PaginaPianta({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
